dbb6f807e55aa1aade22e3791c30d0a545539d0b
Patrick Kodal <patrick@kodal.dev>
2023-12-11 18:45:42 +0100

    improved 2023/11 solutions

--- solution/aoc_day_11.py
+++ solution/aoc_day_11.py
@@ -11,47 +11,32 @@ class AocSolution(AocBaseClass):
 
     def calculate_distances(self, *, part2):
         grid = self.data
-        row_length = len(grid)
-        column_length = len(grid[0])
-        empty_row = []
-        row = 0
-        while row < row_length:
-            empty = True
-            for column in range(column_length):
-                if grid[row][column] == "#":
-                    empty = False
-            if empty:
+        expansion_factor, result = 1e6 - 1 if part2 else 1, 0
+        empty_row, row, empty_column, column = [], 0, [], 0
+        galaxies = []
+        for row, columns in enumerate(grid):
+            if all(value == "." for value in columns):
                 empty_row.append(row)
-            row += 1
-        empty_column = []
-        column = 0
-        while column < column_length:
+        for column, _ in enumerate(grid[0]):
             empty = True
-            for row in range(row_length):
+            for row, _ in enumerate(grid):
                 if grid[row][column] == "#":
+                    galaxies.append((row, column))
                     empty = False
             if empty:
                 empty_column.append(column)
-            column += 1
 
-        galaxies = []
-        for row in range(row_length):
-            for column in range(column_length):
-                if grid[row][column] == "#":
-                    galaxies.append((row, column))
-
-        expansion_factor, result = 10**6 - 1 if part2 else 2 - 1, 0
-        for index1, (row, column) in enumerate(galaxies):
+        for index1, (row1, column1) in enumerate(galaxies):
             for index2 in range(index1, len(galaxies)):
                 row2, column2 = galaxies[index2]
-                distance_i_j = abs(row2 - row) + abs(column2 - column)
+                distance = abs(row2 - row1) + abs(column2 - column1)
                 for current in empty_row:
-                    if min(row, row2) <= current <= max(row, row2):
-                        distance_i_j += expansion_factor
+                    if min(row1, row2) <= current <= max(row1, row2):
+                        distance += expansion_factor
                 for ec in empty_column:
-                    if min(column, column2) <= ec <= max(column, column2):
-                        distance_i_j += expansion_factor
-                result += distance_i_j
+                    if min(column1, column2) <= ec <= max(column1, column2):
+                        distance += expansion_factor
+                result += distance
         return result
 
     def part1(self):
